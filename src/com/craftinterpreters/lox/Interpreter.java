6186cb9258d9b1890cca5409d68fe3a9e3e87ba1
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2024-06-30 18:53:09 +0100

    8.1.3 Statements: expression and print added

--- src/com/craftinterpreters/lox/Interpreter.java
+++ src/com/craftinterpreters/lox/Interpreter.java
@@ -1,6 +1,9 @@
 package com.craftinterpreters.lox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>,
+        Stmt.Visitor<Void> {
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -79,12 +82,13 @@ public class Interpreter implements Expr.Visitor<Object> {
         return null;
     }
 
-    void interpret(Expr expression) {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement: statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
-            Lox.RuntimeError(error);
+            Lox.runtimeError(error);
         }
     }
 
@@ -113,7 +117,8 @@ public class Interpreter implements Expr.Visitor<Object> {
     }
 
     private String stringify(Object object) {
-        if (object == null) return "nil";
+        if (object == null)
+            return "nil";
 
         if (object instanceof Double) {
             String text = object.toString();
@@ -128,5 +133,22 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 }
--- src/com/craftinterpreters/lox/Lox.java
+++ src/com/craftinterpreters/lox/Lox.java
@@ -49,11 +49,11 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
+        List<Stmt> statements = parser.parse();
 
         if (hadError) return;
 
-        interpreter.interpret(expression);
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
@@ -75,7 +75,7 @@ public class Lox {
         }
     }
 
-    static void RuntimeError(RuntimeError error) {
+    static void runtimeError(RuntimeError error) {
         System.err.println(error.getMessage() +
         "\n[line " + error.token.line + "]");
         hadRuntimeError = true;
