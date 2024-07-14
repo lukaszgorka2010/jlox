68abdc6aa992292785f6149967c424360745b12e
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2024-07-14 21:40:10 +0100

    12.4 Get and Set expressions added to classes

--- src/com/craftinterpreters/lox/Interpreter.java
+++ src/com/craftinterpreters/lox/Interpreter.java
@@ -59,6 +59,20 @@ public class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name, 
+                "Only instances have fields.");
+        }
+
+        Object value = evaluate(expr.value);
+        ((LoxInstance)object).set(expr.name, value);
+        return null;
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
@@ -168,6 +182,17 @@ public class Interpreter implements Expr.Visitor<Object>,
         }
 
         return function.call(this, arguments);
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return((LoxInstance) object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name, 
+            "Only instances have properties.");
     }
 
     private void checkNumberOperands(Token operator, Object left, Object right) {
