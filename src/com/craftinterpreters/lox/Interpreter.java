79d9baa2a4e59277b81237c0bd12aebdb08bebca
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2024-07-16 12:51:36 +0100

    12.6 "this" keyword added

--- src/com/craftinterpreters/lox/Interpreter.java
+++ src/com/craftinterpreters/lox/Interpreter.java
@@ -73,6 +73,11 @@ public class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitThisExpr(Expr.This expr) {
+        return lookUpVariable(expr.keyword, expr);
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
