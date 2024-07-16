dbd671c8cd02d1d1e9189f56fc647a8811fa0a69
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2024-07-16 17:47:51 +0100

    13.3.2 Method inheritance and "super" keyword added

--- src/com/craftinterpreters/lox/Interpreter.java
+++ src/com/craftinterpreters/lox/Interpreter.java
@@ -73,6 +73,25 @@ public class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitSuperExpr(Expr.Super expr) {
+        int distance = locals.get(expr);
+        LoxClass superclass = (LoxClass)environment.getAt(
+            distance, "super");
+
+        LoxInstance object = (LoxInstance)environment.getAt(
+            distance-1, "this");
+
+        LoxFunction method = superclass.findMethod(expr.method.lexeme);
+        
+        if (method == null) {
+            throw new RuntimeError(expr.method, 
+            "Undefined property '" + expr.method.lexeme + "'.");
+        }
+
+        return method.bind(object);
+    }
+
+    @Override
     public Object visitThisExpr(Expr.This expr) {
         return lookUpVariable(expr.keyword, expr);
     }
@@ -283,6 +302,11 @@ public class Interpreter implements Expr.Visitor<Object>,
 
         environment.define(stmt.name.lexeme, null);
 
+        if (stmt.superclass != null) {
+            environment = new Environment(environment);
+            environment.define("super", superclass);
+        }
+
         Map<String, LoxFunction> methods = new HashMap<>();
         for (Stmt.Function method: stmt.methods) {
             LoxFunction function = new LoxFunction(method, environment,
@@ -290,7 +314,13 @@ public class Interpreter implements Expr.Visitor<Object>,
             methods.put(method.name.lexeme, function);
         }
 
-        LoxClass klass = new LoxClass(stmt.name.lexeme, (LoxClass)superclass, methods);
+        LoxClass klass = new LoxClass(stmt.name.lexeme,
+        (LoxClass)superclass, methods);
+
+        if (superclass != null) {
+            environment = environment.enclosing;
+        }
+        
         environment.assign(stmt.name, klass);
         return null;
     }
--- src/com/craftinterpreters/lox/Resolver.java
+++ src/com/craftinterpreters/lox/Resolver.java
@@ -59,6 +59,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             resolve(stmt.superclass);
         }
 
+        if (stmt.superclass != null) {
+            beginScope();
+            scopes.peek().put("super", true);
+        }
+
         beginScope();
         scopes.peek().put("this", true);
 
@@ -72,6 +77,9 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         endScope();
+
+        if (stmt.superclass != null) endScope();
+
         currentClass = enclosingClass;
         return null;
     }
@@ -193,6 +201,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.value);
         resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
